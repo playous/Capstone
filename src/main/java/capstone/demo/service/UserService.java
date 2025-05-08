b1b7323e155a95589d79843d8aee227fbdeaaa7e
Minkyu Park <rb6609@naver.com>
2025-05-08 20:50:16 +0900

    [ADD] 로그인 & 회원가입과 관련된 서비스 로직 추가

---
+++ src/main/java/capstone/demo/service/UserService.java
@@ -0,0 +1,61 @@
+package capstone.demo.service;
+
+import capstone.demo.domain.User;
+import capstone.demo.dto.CreateUserDto;
+import capstone.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public long saveUser(CreateUserDto createUserDto) {
+        User user = dtoToUser(createUserDto);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isUserIdDuplicate(String userId) {
+        User existingUser = userRepository.findUserByUserId(userId);
+        if (existingUser != null) log.error("중복 ID = {}", userId);
+        return existingUser != null;
+    }
+
+    @Transactional(readOnly = true)
+    public User authenticate(String userId, String password) {
+        User user = userRepository.findUserByUserId(userId);
+
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        return null;
+
+    }
+
+    public User dtoToUser(CreateUserDto createUserDto) {
+        User user = new User();
+        user.setName(createUserDto.getName());
+        user.setCompany(createUserDto.getCompany());
+        user.setUserId(createUserDto.getUserId());
+        user.setPassword(passwordEncoder.encode(createUserDto.getPassword())); //암호화
+        user.setEmail(createUserDto.getEmail());
+        return user;
+    }
+
+    public long getUserId(CreateUserDto createUserDto) {
+        String userId = createUserDto.getUserId();
+        User user = userRepository.findUserByUserId(userId);
+        return user.getId();
+    }
+
+}
