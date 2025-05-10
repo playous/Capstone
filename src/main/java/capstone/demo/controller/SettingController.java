2568b122089b349c902fd95f713078c094038e9c
Minkyu Park <rb6609@naver.com>
2025-05-10 16:56:42 +0900

    [Feat] 보안기준설정 컨트롤러 추가

---
+++ src/main/java/capstone/demo/controller/SettingController.java
@@ -0,0 +1,62 @@
+package capstone.demo.controller;
+
+import capstone.demo.domain.User;
+import capstone.demo.dto.CriteriaDto;
+import capstone.demo.dto.CriteriaListDto;
+import capstone.demo.service.SettingService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class SettingController {
+
+    private final SettingService settingService;
+
+    @GetMapping("/settings")
+    public String settings(Model model,
+                           HttpSession session) {
+        User user = (User) session.getAttribute("logInUser");
+
+        List<CriteriaDto> criteria = settingService.getCriteriasByUser(user);
+
+        model.addAttribute("criteria", criteria);
+        return "settings";
+    }
+
+    @PostMapping("/settings/save")
+    public String save(@Valid @ModelAttribute CriteriaListDto criteriaList,
+                       BindingResult bindingResult,
+                       HttpSession session,
+                       RedirectAttributes redirectAttributes) {
+
+        User user = (User) session.getAttribute("logInUser");
+
+        if (bindingResult.hasErrors()) {
+            return "settings";
+        }
+
+        try {
+            // 서비스로 데이터 전달
+            settingService.saveCriteria(user, criteriaList.getCriteriaList());
+
+            redirectAttributes.addFlashAttribute("message", "보안 기준이 저장되었습니다.");
+            redirectAttributes.addFlashAttribute("messageType", "success");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "서버 오류");
+            redirectAttributes.addFlashAttribute("messageType", "error");
+        }
+
+        return "redirect:/settings";
+    }
+}
