f17e839bb5fe19952b6ebd092a42e510cdee693f
Minkyu Park <rb6609@naver.com>
2025-05-10 16:51:40 +0900

    [Feat] s3 설정

---
+++ src/main/java/capstone/demo/config/AwsS3Config.java
@@ -0,0 +1,34 @@
+package capstone.demo.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AwsS3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String s3AccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String s3SecretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String s3Region;
+
+    @Bean
+    public S3Client amazonS3() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(s3AccessKey, s3SecretKey);
+
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of(s3Region))
+                .build();
+    }
+
+}
