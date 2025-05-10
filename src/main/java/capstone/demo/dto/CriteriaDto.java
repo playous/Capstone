d5c2b88ad137a3a8a6b8201db3f8fb4bd6cacf95
Minkyu Park <rb6609@naver.com>
2025-05-10 16:51:58 +0900

    [Feat] 보안기준 dto 추가

---
+++ src/main/java/capstone/demo/dto/CriteriaDto.java
@@ -0,0 +1,14 @@
+package capstone.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CriteriaDto {
+
+    @NotBlank(message = "보안 기준명을 입력해주세요.")
+    private String name;
+    private Integer importance;
+}
