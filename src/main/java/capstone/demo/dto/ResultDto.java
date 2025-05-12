aa3de5debdb2ef2dcd79602180b24b2e509b2715
Minkyu Park <rb6609@naver.com>
2025-05-12 20:37:50 +0900

    [Feat] ResultDto 추가 (각각의 보안 요소)

---
+++ src/main/java/capstone/demo/dto/ResultDto.java
@@ -0,0 +1,14 @@
+package capstone.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ResultDto {
+
+    private String name;
+
+    private Integer level;
+
+    private String description;
+
+}
