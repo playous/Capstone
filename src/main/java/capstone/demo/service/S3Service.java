cdcf6efbabcd0b2a790176a8b44beef4e0287e3a
Minkyu Park <rb6609@naver.com>
2025-05-17 13:35:29 +0900

    [Refactor] 서명된 url을 사용하도록 변경

--- src/main/java/capstone/demo/service/S3Service.java
+++ src/main/java/capstone/demo/service/S3Service.java
@@ -1,19 +1,24 @@
 package capstone.demo.service;
 
+import capstone.demo.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
@@ -21,17 +26,25 @@ public class S3Service {
 
     private final S3Client amazonS3;
 
+    private final S3Presigner amazonS3Presigner;
 
     @Value("${app.aws.s3.bucket}")
     private String bucketName;
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        String s3Key = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+    public String uploadFile(MultipartFile file, User user) throws IOException {
+        String s3Key = "documents/" + user.getId() + "/" +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) +
+                "_" + file.getOriginalFilename().replaceAll("\\s+", "_");
 
+        String contentType = file.getContentType();
+
+        if (contentType.startsWith("text/")) {
+            contentType += "; charset=UTF-8";
+        }
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(s3Key)
-                .contentType(file.getContentType())
+                .contentType(contentType)
                 .build();
 
         amazonS3.putObject(putObjectRequest,
@@ -40,20 +53,24 @@ public class S3Service {
         return s3Key;
     }
 
-    public byte[] getFileContent(String s3Key) {
-        try {
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(s3Key)
-                    .build();
+    public String generatePresignedUrl(String s3Key, long expirationTimeMillis) {
+        // GetObjectRequest 생성
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(s3Key)
+                .build();
 
-            ResponseBytes<GetObjectResponse> objectBytes =
-                    amazonS3.getObjectAsBytes(getObjectRequest);
+        // GetObjectPresignRequest 생성
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMillis(expirationTimeMillis))
+                .getObjectRequest(getObjectRequest)
+                .build();
 
-            return objectBytes.asByteArray();
-        } catch (Exception e) {
-            throw new RuntimeException("S3에서 파일 내용을 가져오는 중 오류 발생: " + e.getMessage(), e);
-        }
+        // Presigned URL 생성
+        PresignedGetObjectRequest presignedRequest = amazonS3Presigner.presignGetObject(presignRequest);
+
+        // URL 반환
+        return presignedRequest.url().toString();
     }
 
     public boolean deleteFile(String s3Key) {
