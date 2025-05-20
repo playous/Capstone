6990924e86b633e0f251f145e5c62e55b7d2448a
Minkyu Park <rb6609@naver.com>
2025-05-20 18:28:49 +0900

    [Fix] CriteriaListDto에 기본 생성자 추가 - 폼 바인딩 과정에서 발생하는 객체 생성 오류 해결

--- src/main/java/capstone/demo/dto/CriteriaListDto.java
+++ src/main/java/capstone/demo/dto/CriteriaListDto.java
@@ -3,10 +3,13 @@ package capstone.demo.dto;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CriteriaListDto {
 
     @Valid
