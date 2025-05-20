ca498b08919632e9385f6ff154ab852dbec319d6
Minkyu Park <rb6609@naver.com>
2025-05-20 18:28:34 +0900

    [Fix] CriteriaDto에 기본 생성자 추가 - 폼 바인딩 과정에서 발생하는 객체 생성 오류 해결

--- src/main/java/capstone/demo/dto/CriteriaDto.java
+++ src/main/java/capstone/demo/dto/CriteriaDto.java
@@ -3,9 +3,11 @@ package capstone.demo.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CriteriaDto {
 
     @NotBlank(message = "보안 요소를 입력해주세요.")
