b1d14326f162fae0ddc8a9f3caf7b22521a7f309
Minkyu Park <rb6609@naver.com>
2025-05-21 12:15:27 +0900

    [Feat] ResultListDto에 문서ID 추가

--- src/main/java/capstone/demo/dto/ResultListDto.java
+++ src/main/java/capstone/demo/dto/ResultListDto.java
@@ -7,6 +7,8 @@ import java.util.List;
 @Data
 public class ResultListDto {
 
+    private Long documentId;
+
     private List<ResultDto> resultList;
 
     private Integer documentSecurityLevel;  // 문서 전체 보안 등급
