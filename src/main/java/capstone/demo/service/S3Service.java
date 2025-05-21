aaebf9a80b14aa253b7de2e657f26a08939817f1
Minkyu Park <rb6609@naver.com>
2025-05-21 12:16:06 +0900

    [Feat] AWS 서명 URL 만료 시간 변경 (30분)

--- src/main/java/capstone/demo/service/S3Service.java
+++ src/main/java/capstone/demo/service/S3Service.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -62,7 +61,7 @@ public class S3Service {
 
         // GetObjectPresignRequest 생성
         GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMillis(expirationTimeMillis))
+                .signatureDuration(Duration.ofMinutes(expirationTimeMillis))
                 .getObjectRequest(getObjectRequest)
                 .build();
 
